c944313d58bc75e44fe2c8fb1898e25b831bebb3
Steven Chau <steven.p.chau@gmail.com>
2014-09-09 15:52:47 -0700

    Spec event_get and event_update

--- lib/eventbrite-legacy/legacy/rest/events.rb
+++ lib/eventbrite-legacy/legacy/rest/events.rb
@@ -1,5 +1,6 @@
 require 'eventbrite-legacy/legacy/rest/utils'
 require 'eventbrite-legacy/legacy/action'
+require 'eventbrite-legacy/legacy/event'
 
 module Eventbrite
   module Legacy
@@ -7,11 +8,22 @@ module Eventbrite
       module Events
         include Eventbrite::Legacy::REST::Utils
 
+        def event_get(event, options = {})
+          options[:id] = extract_id(event)
+
+          perform_with_nested_object(:get, :v1, "/json/event_get/", options, :event, Eventbrite::Legacy::Event)
+        end
+
         def event_copy(event, title, options = {})
           options[:id]    = extract_id(event)
           options[:title] = title
 
           perform_with_nested_object(:get, :v1, "/json/event_copy/", options, :process, Eventbrite::Legacy::Action)
+        end
+
+        def event_update(event, options = {})
+          options[:id] = extract_id(event)
+          perform_with_nested_object(:get, :v1, "/json/event_update/", options, :process, Eventbrite::Legacy::Action)
         end
       end
     end
