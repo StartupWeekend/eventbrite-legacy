03f861b4e8ecc02608e952c7d29fd9c68976d28a
Steven Chau <steven.p.chau@gmail.com>
2015-01-20 15:22:55 -0800

    Convert ID to a string

--- lib/eventbrite-legacy/legacy/action.rb
+++ lib/eventbrite-legacy/legacy/action.rb
@@ -1,6 +1,11 @@
 module Eventbrite
   module Legacy
     class Action < Hashie::Mash
+      # :status, :message, :id, :method
+      def id
+        fetch("id").to_s
+      end
+
       def success?
         status == "OK"
       end
