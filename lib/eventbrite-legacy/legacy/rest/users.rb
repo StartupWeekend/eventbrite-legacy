6a6e823e6515084a1304eb37da57d2fba33613a1
Steven Chau <steven.p.chau@gmail.com>
2015-01-20 15:22:36 -0800

    Import User List Events

--- lib/eventbrite-legacy/legacy/rest/users.rb
+++ lib/eventbrite-legacy/legacy/rest/users.rb
@@ -1,5 +1,6 @@
 require 'eventbrite-legacy/legacy/rest/utils'
 require 'eventbrite-legacy/legacy/user'
+require 'eventbrite-legacy/legacy/event'
 
 module Eventbrite
   module Legacy
@@ -19,6 +20,10 @@ module Eventbrite
           options.merge!(email: email, passwd: password)
 
           perform_with_nested_object(:get, :v1, "/json/user_new/", options, :user, Eventbrite::Legacy::Action)
+        end
+
+        def user_list_events(options = {})
+          perform_with_nested_cursor(:get, :v1, "/json/user_list_events/", options, :event, :events, Eventbrite::Legacy::Event)
         end
       end
     end
--- lib/eventbrite-legacy/legacy/rest/utils.rb
+++ lib/eventbrite-legacy/legacy/rest/utils.rb
@@ -17,6 +17,17 @@ module Eventbrite
           request = Eventbrite::Legacy::Request.new(self, request_method, version, path, container, options)
           request.perform_with_object(klass)
         end
+
+        # @param request_method [Symbol]
+        # @param version [Symbol]
+        # @param path [String]
+        # @param options [Hash]
+        # @param klass [Class]
+        def perform_with_nested_cursor (request_method, version, path, options, container, collection_name, klass = nil) # rubocop:disable ParameterLists
+          merge_default_cursor!(options)
+          request = Eventbrite::Legacy::Request.new(self, request_method, version, path, container, options)
+          request.perform_with_cursor(collection_name.to_sym, klass)
+        end
       end
     end
   end
